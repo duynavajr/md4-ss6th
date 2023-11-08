913b8695aa090272055011280b9085a063e38e92
duynavajr <132381004+duynavajr@users.noreply.github.com>
2023-11-08 08:08:24 +0700

    Add files via upload

---
+++ MD4_ss6th1-main/src/main/java/com/example/server_time/login.java
@@ -0,0 +1,44 @@
+package com.example.server_time;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "LoginServlet", value = "/login")
+public class login extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Your doGet() implementation
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+
+        if ("admin".equals(username) && "admin".equals(password)) {
+            writer.println("<h1>Welcome " + username + " to website</h1>");
+        } else {
+            writer.println("<h1>Login Error</h1>");
+        }
+
+        writer.println("</html>");
+    }
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+}
